afa9b109bf1cbeca9fda00c90704112633e57e2b
astapt3final <astapt3final@gmail.com>
2025-11-20 21:25:50 +0530

    Initial commit

---
+++ mltest.py
@@ -0,0 +1,10 @@
+import torch
+import transformers 
+import onnxruntime 
+import flask
+
+print("----------------------------------------")
+print("SUCCESS: all ai systems are online")
+print(f"torch version : {torch.__version__}")
+print(f"transformers version: {transformers.__version__}")
+print("------------------------------------------------------")
